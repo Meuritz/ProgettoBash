9b7d5acc9df0c19897f1cc87970ce029cfbf3e3a
cristiano <cristiano.moscetti@gmail.com>
2023-12-14 13:06:26 +0100

    versione senza ricorsiva

--- update.sh
+++ update.sh
@@ -3,13 +3,14 @@
 #prendo come parametri le dir e le assegno a due variabili
 dir1="$1"
 dir2="$2"
+parametro="$3"
 
 #assegno l'output di dirname a due variabili
 full_dir1=$(dirname "$dir1")
 full_dir2=$(dirname "$dir2")
 
 #controllo che i parametri inseriti siano due
-if [[ "$#" -ne 2 ]]; then
+if [[ "$#" -lt 2 ]]; then
     echo "inserisci due parametri!!!"
     exit
 fi
@@ -53,20 +54,65 @@ for files in "$dir2"/*
         ((i++))
     done
 
-#ciclo for per iterare i file della prima dir e della seconda
+#ciclo for per iterare i file della prima dir
 #funziona in maniera unidirezionale quindi ciclo i file della prima e non viceversa
-for i in "${!files_dir1[@]}" 
-    do
-        #controllo che il file sia un file regolare
-        if [[ -f ${files_dir1[i]} ]]; then
-            #controllo che il file sia gia presente, se lo e' controllo se il file che voglio copiare
-            #e' piu' recente del secondo se lo e' lo copio senno vado avanti
-            if [[ (${files_dir1[i]} == ${files_dir2[i]}) && (${files_dir1[i]} -nt ${files_dir2[i]}) ]]; then
-                cp "${files_dir1[i]}" "$dir2"
+
+case "$parametro" in
+    [-R][-r]* )
+    
+    ;;
+    [-I][-i]* )
+        for i in "${!files_dir1[@]}"
+        do
+            #controllo che il file sia un file regolare
+            if [[ -f ${files_dir1[i]} ]]; then
+                    #controllo che il file sia gia presente, se lo e' controllo se il file che voglio copiare
+                    #e' piu' recente del secondo se lo e' lo copio senno vado avanti
+                if [[ (${files_dir1[i]} == ${files_dir2[i]}) && (${files_dir1[i]} -nt ${files_dir2[i]}) ]]; then
+                    while true; do
+                        read -p "Vuoi sovrascrivere il file ${files_dir1[i]} con il file ${files_dir2[i]}? y/n" yesno
+                            case $yesno in
+                                [Yy]* ) cp "${files_dir1[i]}" "$dir2"; break;;
+                                [Nn]* ) break;;
+                                    * ) echo "inserisci y(si) o n(no)!!!";;
+                            esac
+                    done
+                fi
+                #copio i file che non sono gia presenti
+                if [[ ${files_dir1[i]} != ${files_dir2[i]} ]]; then
+                    while true; do
+                        echo "Vuoi copiare il file ${files_dir1[i]} in $dir2? y/n"
+                        read yesno
+                            case $yesno in
+                                [Yy]* ) cp "${files_dir1[i]}" "$dir2"; break;;
+                                [Nn]* ) break;;
+                                    * ) echo "inserisci y(si) o n(no)!!!";;
+                            esac
+                    done
+                fi
+            fi    
+        done
+
+    ;;
+    [-IR][-ir][-RI][-ri]* )
+
+    ;;
+    *)
+    for i in "${!files_dir1[@]}" 
+        do
+            #controllo che il file sia un file regolare
+            if [[ -f ${files_dir1[i]} ]]; then
+                #controllo che il file sia gia presente, se lo e' controllo se il file che voglio copiare
+                #e' piu' recente del secondo se lo e' lo copio senno vado avanti
+                if [[ (${files_dir1[i]} == ${files_dir2[i]}) && (${files_dir1[i]} -nt ${files_dir2[i]}) ]]; then
+                    cp "${files_dir1[i]}" "$dir2"
+                fi
+                #copio i file che non sono gia presenti
+                if [[ ${files_dir1[i]} != ${files_dir2[i]} ]]; then
+                    cp "${files_dir1[i]}" "$dir2"
+                fi
             fi
-            #copio i file che non sono gia presenti
-            if [[ ${files_dir1[i]} != ${files_dir2[i]} ]]; then
-                cp "${files_dir1[i]}" "$dir2"
-            fi
-        fi
-    done
+        done
+    ;;
+    
+esac
